a58cc0e2a09d4220f31e55d2b1cbdee2a005dd9b
N30012000 <zuberinoosha@outlook.com>
2026-04-27 13:40:28 +0500

    Create airports.py

---
+++ airports.py
@@ -0,0 +1,22 @@
+# airports.py
+# Centralized list of Air Sial operational airports
+AIR_SIAL_AIRPORTS = [
+    {"code":"OPSK","name":"Sialkot","country":"Pakistan"},
+    {"code":"OPKC","name":"Karachi","country":"Pakistan"},
+    {"code":"OPLA","name":"Lahore","country":"Pakistan"},
+    {"code":"OPIS","name":"Islamabad","country":"Pakistan"},
+    {"code":"OPPS","name":"Peshawar","country":"Pakistan"},
+    {"code":"OPQT","name":"Quetta","country":"Pakistan"},
+    {"code":"OPFA","name":"Faisalabad","country":"Pakistan"},
+    {"code":"OPMT","name":"Multan","country":"Pakistan"},
+    {"code":"OMDB","name":"Dubai","country":"UAE"},
+    {"code":"OMSJ","name":"Sharjah","country":"UAE"},
+    {"code":"OMAA","name":"Abu Dhabi","country":"UAE"},
+    {"code":"OERK","name":"Riyadh","country":"Saudi Arabia"},
+    {"code":"OEJN","name":"Jeddah","country":"Saudi Arabia"},
+    {"code":"OEDF","name":"Dammam","country":"Saudi Arabia"},
+    {"code":"OTHH","name":"Doha","country":"Qatar"},
+    {"code":"OBBI","name":"Bahrain","country":"Bahrain"},
+    {"code":"OOMS","name":"Muscat","country":"Oman"},
+    {"code":"OKBK","name":"Kuwait","country":"Kuwait"},
+]
